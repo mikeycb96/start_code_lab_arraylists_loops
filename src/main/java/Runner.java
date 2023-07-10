db6c9038d825f55d06d93bc59c64648638281192
mikeycb96 <michaelsonbaptist@gmail.com>
2023-07-10 17:32:10 +0100

    added extension

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -46,15 +46,53 @@ public class Runner {
         System.out.println("numbers: " + numbers);
 
 //        1. Print out a list of the even integers
+        for (int number: numbers
+             ) if(number % 2 == 0) {
+            System.out.println(number);
+        }
 //        2. Print the difference between the largest and smallest value
+//        Collections.sort(numbers);
+//        System.out.println(numbers.get(10) - numbers.get(0));
+        System.out.println(Collections.max(numbers)- Collections.min(numbers));
+//      Want to find out how to .get max and min number
 //        3. Print True if the list contains a 1 next to a 1 somewhere.
+
+        boolean sameNumber = false;
+
+        for(int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i) == 1 && numbers.get(i+1) == 1){
+                sameNumber = true;
+                break;
+
+            }
+
+
+        }
+        System.out.println(sameNumber);
 //        4. Print the sum of the numbers,
+        int sum = 0;
+
+        for (int number : numbers){
+            sum += number;
+        }
+        System.out.println(sum);
+
+
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count.
 //
 //          So [2, 7, 13, 2] would have sum of 9.
+        int sumWithoutThirteen = 0;
 
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) != 13 &&( i == 0 || numbers.get(i-1) != 13)){
+                sumWithoutThirteen += numbers.get(i);
+            }
+
+        }
+
+        System.out.println(sumWithoutThirteen);
     }
 
 }
